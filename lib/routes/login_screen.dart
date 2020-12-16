761099f2aa020fecafeec1a00bb1ebe90895caf1
ranawageh <62141987+ranawageh@users.noreply.github.com>
2020-12-16 16:22:26 +0200

    updates

---
+++ lib/routes/login_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:e_commerce/constance.dart';
+import 'package:e_commerce/routes/signup_screen.dart';
+import 'package:e_commerce/widgets/Custom_TextField.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
+  static String id='LoginScreen';
+  @override
+  Widget build(BuildContext context) {
+       double height = MediaQuery.of(context).size.height;
+        return Scaffold(
+        backgroundColor: KMainColor,
+          body: ListView(
+            children:<Widget>[
+            SizedBox(
+               height: height*.2,
+             ),
+             CustomTextField(
+                hint: 'Enter your email',
+                icon: Icons.email,
+              ),
+             SizedBox(
+               height: height*.02,
+             ), 
+              CustomTextField(
+                 icon: Icons.lock,
+                 hint: 'Enter your password',
+                 ),
+                SizedBox(
+               height: height*.05,
+             ),  
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 120,vertical:30),
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                    ),
+                   onPressed: () {  },
+                  color: Colors.black,
+                  
+                   child: Text(
+                     'Login',
+                     style: TextStyle(
+                       color: Colors.white,
+                     ),
+                     ),
+                   ),
+              ) ,
+               SizedBox(
+               height: height*.01,
+             ),
+               Row(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: <Widget>[
+                   Text("Don't have an account ? ",
+                   style: TextStyle(
+                     color:Colors.white,
+                     fontSize: 16
+                     ),
+                   ),
+                   GestureDetector(
+                     onTap: (){
+                       Navigator.pushNamed(context, signup_screen.id);
+                     },
+                     child: Text('Sign up',
+                     style: TextStyle(
+                       fontSize: 16
+                     ) ,
+                     ),
+                   )
+                 ],
+               )
+        ],
+      ),
+    );
+  }
+}
